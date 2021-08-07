8f264dcf497c9d5fe1a6b25ba853404db703c581
caarlosdamian <dapecarlos@gmail.com>
2021-08-07 11:35:47 -0500

    add map component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab/Rating";
+import useStyles from "./styles";
+import mapStyles from "../../mapStyles";
 
 const Map = () => {
-    return (
-        <div>
-            <h1>Map</h1>
-        </div>
-    )
-}
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width:600px)");
+  const coords = { lat: 0, lng: 0 };
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={coords}
+        center={coords}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
+};
 
-export default Map
+export default Map;
