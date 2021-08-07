959b6ffe2c8d32e405306ae9caed402d6c728d38
caarlosdamian <dapecarlos@gmail.com>
2021-08-06 20:12:23 -0500

    add structure and components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
 
 const App = () => {
-    return (
-        <div>
-            <h1>Hello, World!</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+};
 
-export default App
+export default App;
